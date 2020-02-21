adca1850da12b555fb7e49a555ec52c191d88d61
FrankLA0203 <frankla0203@outlook.com>
2020-02-21 16:29:50 +0800

    Patch fix

--- x.c
+++ x.c
@@ -47,15 +47,15 @@ typedef struct {
 } Key;
 
 enum resource_type {
-    STRING = 0;
-    INTEGER = 1;
-    FLOAT = 2;
+    STRING = 0,
+    INTEGER = 1,
+    FLOAT = 2
 };
 
 typedef struct {
     char *name;
     enum resource_type type;
-    void +dst;
+    void *dst;
 } ResourcePref;
 
 /* X modifiers */
