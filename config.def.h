419bfdd3532dab2621915283fb40eae553f16bfb
FrankLA0203 <frankla0203@outlook.com>
2020-03-29 01:20:09 +0800

    Bind change

--- config.def.h
+++ config.def.h
@@ -196,9 +196,9 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
-    { MODKEY,               XK_u,           externalpipe,   {.v = openurlcmd } },
+        { MODKEY,               XK_o,           externalpipe,   {.v = openurlcmd } },
 	{ MODKEY,               XK_y,           externalpipe,   {.v = copyurlcmd } },
-	{ MODKEY,               XK_o,           externalpipe,   {.v = copyoutput } },
+	{ MODKEY,               XK_u,           externalpipe,   {.v = copyoutput } },
 };
 
 /*
